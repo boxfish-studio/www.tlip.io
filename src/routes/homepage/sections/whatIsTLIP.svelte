6e1bcbb691a0e75583efbfc8c67fc44ac1d56932
Jean Carlo Noguera <jcnoguera@boxfish.studio>
2022-06-13 13:26:32 +0200

    Update code as reviewed

--- src/routes/homepage/sections/whatIsTLIP.svelte
+++ src/routes/homepage/sections/whatIsTLIP.svelte
@@ -4,9 +4,9 @@
     export let id: string;
 </script>
 
-<section class="px-12 md:px-24 lg:px-32 py-32 lg:py-72" {id}>
+<section class="container mt-32 mb-36" {id}>
     <h2 class="text-center text-36 mb-9">What is TLIP?</h2>
-    <p class="text-center font-normal text-16 leading-6 lg:px-64 tracking-0.04 mb-32">
+    <p class="text-center leading-6 lg:px-52 tracking-0.04 mb-32">
         It is not a platform but a collaborative infrastructure, where all actors can share data and documents. Used by
         both border agencies and commercial actors, each party has full control of its own data, and all data available
         directly from the source.
@@ -18,9 +18,9 @@
                     <img src="./assets/{image}" alt={title} class="image-shadow">
                 </div>
                 <div class="w-full xl:w-7/12 md:{idx % 2 ? 'pr': 'pl'}-28 flex flex-col md:items-start items-center justify-center text-center md:text-left">
-                    <h4 class="font-bold text-15 lg:text-20 tracking-0.02 leading-6 capitalize lg:text-left">{eyebrow}</h4>
-                    <p class="font-bold text-24 lg:text-32 tracking-normal lg:tracking-0.02 leading-7 md:leading-10 md:my-3 my-2">{title}</p>
-                    <p class="font-normal text-12 lg:text-16 leading-6 tracking-0.04 ">{description}</p>
+                    <h4 class="font-bold text-15 lg:text-20 tracking-0.02 leading-6 capitalize lg:text-left text-grey-500">{eyebrow}</h4>
+                    <h3 class="text-24 lg:text-32 tracking-normal lg:tracking-0.02 md:my-3 my-2">{title}</h3>
+                    <p class="text-12 lg:text-16 leading-6 tracking-0.04 text-grey-500">{description}</p>
                 </div>
             </div>
         </div>
