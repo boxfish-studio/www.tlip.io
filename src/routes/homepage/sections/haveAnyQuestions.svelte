2ae71d6ab7901038f8b55ae0570f20a1cd6e3a12
Jean Carlo Noguera <jcnoguera@boxfish.studio>
2022-06-13 10:38:54 +0200

    Update HaveAnyQeustions section

--- src/routes/homepage/sections/haveAnyQuestions.svelte
+++ src/routes/homepage/sections/haveAnyQuestions.svelte
@@ -16,7 +16,7 @@
     ];
 </script>
 
-<section class="px-10 md:px-32 py-20 bg-blue-300" {id}>
+<section class="px-6 md:px-32 py-20 bg-blue-300" {id}>
     <div class="flex text-white justify-between flex-col text-center lg:flex-row lg:text-left">
         <div class="max-w-2xl lg:mr-7">
             <h1 class="capitalize mb-6 flex text-36 md:text-64 justify-center">Have any questions?</h1>
@@ -24,7 +24,7 @@
                 New to TLIP and got questions? Check out our FAQ or contact us by email, we will get back to you as soon
                 as we can.
             </p>
-            <div class="buttons w-full flex gap-x-7 mt-20">
+            <div class="buttons w-full flex gap-x-7 mt-10 lg:mt-20">
                 {#each BUTTONS as button}
                     <div class="w-2/4 uppercase">
                         <Button classes="text-16 font-bold h-14" {...button} small secondary />
@@ -33,8 +33,7 @@
             </div>
         </div>
         <div class="w-full lg:max-w-md flex relative object-scale-down items-center mt-20 lg:mt-0 justify-center">
-            <img src="./assets/have-questions-outter.png" alt="" />
-            <img src="./assets/have-questions-inner.png" alt="" class="absolute w-4/12 object-scale-down" />
+            <img src="./assets/haveAnyQuestions.png" alt="TLIP in Africa" />
         </div>
     </div>
 </section>
@@ -43,13 +42,6 @@
     .buttons {
         @screen lg {
             max-width: 412px;
-        }
-    }
-    img {
-        &.absolute {
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
         }
     }
 </style>
