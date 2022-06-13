01b6a693ca96e524b75e9a6b932a7129ea3e4274
Jean Carlo Noguera <jcnoguera@boxfish.studio>
2022-06-13 10:41:17 +0200

    Move Have Any Questions section to its own section folder

--- src/sections/haveAnyQuestions.svelte
+++ src/sections/haveAnyQuestions.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import type { Button as ButtonType } from "$lib/types/components";
-    import { Button } from "../../../components";
+    import { Button } from "../components";
 
     export let id: string;
 
