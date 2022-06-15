470b06a5c508a4414d21fa1e16686940a4b4ada5
Jean Carlo Noguera <jcnoguera@boxfish.studio>
2022-06-15 17:21:53 +0200

    Add Carousel to partner logos

--- src/routes/homepage/sections/partners.svelte
+++ src/routes/homepage/sections/partners.svelte
@@ -9,46 +9,44 @@
 <div class="flex flex-col justify-center items-center pt-20">
     <div class="container">
         <h4 class="font-bold mb-10 text-center">{GOVERNMENT_PARTNERS.title}</h4>
-        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-10 w-full justify-items-center">
-            {#each GOVERNMENT_PARTNERS.logos as logo}
-                <div class="w-36 h-36 border border-grey-100 rounded p-4">
-                    <img src={logo} alt={logo} class="w-full h-full object-contain" />
-                </div>
-            {/each}
-        </div>
+            <div class="mx-auto max-w-4xl">
+                <Swiper
+                    slidesPerView={"auto"}
+                    centeredSlides={true}
+                    spaceBetween={40}
+                    loop={true}
+                    autoplay={true}
+                    class="mySwiper"
+                    modules={[Autoplay]}
+                >
+                {#each GOVERNMENT_PARTNERS.logos as logo}
+                <SwiperSlide>
+                    <div class="w-36 h-36 border border-grey-100 rounded p-4">
+                        <img src={logo} alt={logo} class="w-full h-full object-contain" />
+                    </div>
+                </SwiperSlide>
+                {/each}
+                </Swiper>
+            </div>
     </div>
     <h4 class="container font-bold mt-9 mb-4 text-center">{COMMERCIAL_PARTNERS.title}</h4>
-    <Swiper
-        breakpoints={{
-            580: {
-                slidesPerView: 2
-            },
-            750: {
-                slidesPerView: 4
-            },
-            960: {
-                slidesPerView: 5
-            },
-            1200: {
-                slidesPerView: 6
-            },
-            1600: {
-                slidesPerView: 8
-            }
-        }}
-        slidesPerView={2}
-        spaceBetween={30}
-        loop={true}
-        autoplay={true}
-        class="mySwiper"
-        modules={[Autoplay]}
-    >
-        {#each COMMERCIAL_PARTNERS.logos as logo}
-            <SwiperSlide>
-                <div class="w-36 h-36 border border-grey-100 rounded p-4 mx-auto">
-                    <img src={logo} alt={logo} class="w-full h-full object-contain" />
-                </div>
-            </SwiperSlide>
-        {/each}
-    </Swiper>
+        <div class="swiper-block w-full px-6 lg:px-0">
+            <Swiper
+                slidesPerView={"auto"}
+                centeredSlides={true}
+                spaceBetween={40}
+                loop={true}
+                autoplay={true}
+                class="mySwiper"
+                modules={[Autoplay]}
+            >
+                {#each COMMERCIAL_PARTNERS.logos as logo}
+                    <SwiperSlide>
+                        <div class="w-36 h-36 border border-grey-100 rounded p-4">
+                            <img src={logo} alt={logo} class="w-full h-full object-contain" />
+                        </div>
+                    </SwiperSlide>
+                {/each}
+            </Swiper>
+        </div>
 </div>
