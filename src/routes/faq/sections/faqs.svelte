66b62d74f5b310fa32a1dbd60ab9d90d37b14db4
evavirseda <evirseda@boxfish.studio>
2022-06-15 18:15:14 +0200

    Update faqs component and styles

--- src/routes/faq/sections/faqs.svelte
+++ src/routes/faq/sections/faqs.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
     import { FAQS as data } from "./faqs";
-    import { DropdownFaqs } from "./";
+    import { Accordion } from "../../../components";
 </script>
 
 <section class="pt-12 pb-32 relative z-20">
     <div class="container">
-        <DropdownFaqs {data} isExpandable />
+        <div class="w-full">
+            <div id="contents">
+                {#each data?.sections as section, index}
+                    <div
+                        class={`text-32 md:text-36 leading-125 md:leading-110 tracking-0.02 font-bold mb-5 md:mb-10 text-blue-600 ${
+                            index > 0 && "mt-10 md:mt-20"
+                        }`}
+                    >
+                        {section.title}
+                    </div>
+                    <div id={section.id} class="border border-grey-100 rounded-2xl bg-white p-7 md:p-9">
+                        <Accordion items={section.subsections} />
+                    </div>
+                {/each}
+            </div>
+        </div>
     </div>
 </section>
 
