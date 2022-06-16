b6130cff86b3a4e5fdb187c2a51f673dd0d792ce
Jean Carlo Noguera <jcnoguera@boxfish.studio>
2022-06-16 13:14:59 +0200

    fix: Center carousels

--- src/routes/homepage/sections/partners.svelte
+++ src/routes/homepage/sections/partners.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex flex-col justify-center items-center space-y-8 pt-20">
     {#each PARTNERS as partnerCategory}
-        <div class="w-full">
+        <div class="w-full flex flex-col justify-center items-center">
             <h4 class="font-bold mb-10 text-center">{partnerCategory.title}</h4>
             <Carousel images={partnerCategory.logos} />
         </div>
