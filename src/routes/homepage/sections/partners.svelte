9e0344f7eb35b8df8284e4202334f81026fab1b3
evavirseda <evirseda@boxfish.studio>
2022-06-16 16:17:23 +0200

    Add overflow hidden

--- src/routes/homepage/sections/partners.svelte
+++ src/routes/homepage/sections/partners.svelte
@@ -3,7 +3,7 @@
     import { PARTNERS } from "./partners";
 </script>
 
-<div class="flex flex-col justify-center items-center space-y-9 pt-20">
+<div class="flex flex-col justify-center items-center space-y-9 pt-20 overflow-hidden">
     {#each PARTNERS as partnerCategory}
         <div class="w-full flex flex-col justify-center items-center">
             <h4 class="font-bold mb-4 text-center">{partnerCategory.title}</h4>
