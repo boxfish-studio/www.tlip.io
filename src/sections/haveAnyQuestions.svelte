911e24d88a1fa9e409c1ef974988d06abab1ac25
evavirseda <evirseda@boxfish.studio>
2022-06-16 16:09:29 +0200

    Update title and styles

--- src/sections/haveAnyQuestions.svelte
+++ src/sections/haveAnyQuestions.svelte
@@ -27,15 +27,15 @@
         <div class="max-w-2xl lg:mr-7">
             <h1 class="capitalize mb-6 flex text-36 md:text-64 justify-center lg:justify-start">{HEADING.title}</h1>
             <p>{HEADING.description}</p>
-            <div class="buttons w-full flex gap-x-7 mt-10 lg:mt-20">
+            <div class="buttons w-full md:flex gap-x-7 mt-6 md:mt-10 lg:mt-28">
                 {#each BUTTONS as button}
-                    <div class="w-2/4 uppercase">
+                    <div class="w-2/4 uppercase mx-auto mb-6 las:mb-0 md:mb-0">
                         <Button classes="text-16 font-bold h-14" {...button} small secondary />
                     </div>
                 {/each}
             </div>
         </div>
-        <div class="w-full lg:max-w-md flex relative object-scale-down items-center mt-20 lg:mt-0 justify-center">
+        <div class="w-full lg:max-w-md flex relative object-scale-down items-center mt-12 lg:mt-0 justify-center">
             <img src="./assets/haveAnyQuestions.png" alt="TLIP in Africa" />
         </div>
     </div>
